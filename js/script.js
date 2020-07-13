a38696eca606aaca48b3909ac8eac72557847d93
Unknown <fioremartina@hotmail.com>
2020-07-13 15:43:05 +0200

    no logs

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ var cognomeup = cognome.charAt(0).toUpperCase() + cognome.slice(1).toLowerCase()
 cognomi.push(cognomeup);
 var lista = cognomi.sort();
 
-console.log(cognomi);
-console.log(cognomeup);
 
 for (var i = 0; i < lista.length; ++i) {
   document.getElementById("lista").innerHTML += "<li>" + lista[i] + "</li>" ;
