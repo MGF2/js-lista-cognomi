54eb236d767885b41dfc99513d8a21d449944cad
Unknown <fioremartina@hotmail.com>
2020-07-13 16:09:50 +0200

    con while

--- js/script.js
+++ js/script.js
@@ -4,7 +4,14 @@ var cognomeup = cognome.charAt(0).toUpperCase() + cognome.slice(1).toLowerCase()
 cognomi.push(cognomeup);
 var lista = cognomi.sort();
 
-
 for (var i = 0; i < cognomi.length; ++i) {
-  document.getElementById("lista").innerHTML += "<li>" + lista[i] + "</li>" ;
+document.getElementById("lista").innerHTML += "<li>" + lista[i] + "</li>" ;
+}
+
+// CON WHILE
+var i = 0;
+
+while (i < cognomi.length) {
+  document.getElementById("lista2").innerHTML += "<li>" + lista[i] + "</li>" ;
+  ++i;
 }
